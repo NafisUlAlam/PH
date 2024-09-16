af6939d6167d72bd6b17049317aa2d3f2a5b7294
Nafis <ahmednafis74@gmail.com>
2024-09-16 23:11:43 +0600

    added all button background colors

--- practice/scripts/first.js
+++ practice/scripts/first.js
@@ -6,3 +6,18 @@ const redButton = document.getElementById("make-red");
 redButton.addEventListener("click", (event) => {
   body.style.backgroundColor = "rgba(120, 0, 0, 0.5)";
 });
+
+const greenButton = document.getElementById("make-green");
+greenButton.addEventListener("click", (event) => {
+  body.style.backgroundColor = "rgba(0, 120, 0, 0.5)";
+});
+
+const blueButton = document.getElementById("make-blue");
+blueButton.addEventListener("click", (event) => {
+  body.style.backgroundColor = "rgba(0, 0, 120, 0.5)";
+});
+
+const yellowButton = document.getElementById("make-yellow");
+yellowButton.addEventListener("click", (event) => {
+  body.style.backgroundColor = "yellow";
+});
