b3b72a616af5667f1100f3572400730f4daebbca
Nafis <ahmednafis74@gmail.com>
2024-09-16 23:09:29 +0600

    added eventlistener on make red button

--- practice/scripts/first.js
+++ practice/scripts/first.js
@@ -1,2 +1,8 @@
 console.log("hello from external js file");
 console.log(document);
+const body = document.getElementsByTagName("body")[0];
+
+const redButton = document.getElementById("make-red");
+redButton.addEventListener("click", (event) => {
+  body.style.backgroundColor = "rgba(120, 0, 0, 0.5)";
+});
