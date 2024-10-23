c7347b0925cb5c0c44e39ce04f0b77606e62ec6b
Nafis <ahmednafis74@gmail.com>
2024-10-23 13:43:42 +0600

    added blogs component

---
+++ React/module-42/src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold text-white bg-gray-700">
+        Knowledge Cafe
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
